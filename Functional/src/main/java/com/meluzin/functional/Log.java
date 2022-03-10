8a7218cecf25feb726d58fb0fb4821edc0407a32
Jiri Meluzin <meluzinj>
2022-03-10 12:20:51 +0100

    Log refactoring and default to info

--- Functional/src/main/java/com/meluzin/functional/Log.java
+++ Functional/src/main/java/com/meluzin/functional/Log.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.logging.ConsoleHandler;
@@ -27,72 +28,69 @@ public class Log {
 			if (paramLogRecord.getThrown() != null) {
 				try (StringWriter sw = new StringWriter()) {
 					try (PrintWriter pw = new PrintWriter(sw)) {
-		
+
 						paramLogRecord.getThrown().printStackTrace(pw);
 						stackTrace = sw.toString();
-					} 
+					}
 				} catch (IOException e) {
 				}
 			}
-			return LOG_DATETIME_FORMATTER.format(date)+ " [" + paramLogRecord.getSourceClassName()+":" + paramLogRecord.getSourceMethodName() + "] [" + paramLogRecord.getLevel() + "] " + paramLogRecord.getMessage() + stackTrace + "\n";
+			return LOG_DATETIME_FORMATTER.format(date) + " [" + paramLogRecord.getSourceClassName() + ":"
+					+ paramLogRecord.getSourceMethodName() + "] [" + paramLogRecord.getLevel() + "] "
+					+ paramLogRecord.getMessage() + stackTrace + "\n";
 		}
-		
+
 	};
+
 	public static void main(String[] args) {
-		 System.out.println(Log.LOG_DATETIME_FORMATTER.format(new Date()));
-		 System.out.println(Log.XSD_DATETIME_FORMATTER.format(new Date()));
-		 //Log.addHandler(new SystemOutHandler());
-		 Log.get().severe("started");
-		 Log.get().severe("finished");
-		 Log.get().info("finished");
-		 Log.get().fine("abc");
+		System.out.println(Log.LOG_DATETIME_FORMATTER.format(new Date()));
+		System.out.println(Log.XSD_DATETIME_FORMATTER.format(new Date()));
+		Log.get().severe("started");
+		Log.get().severe("finished");
+		Log.get().info("finished");
+		Log.get().fine("abc");
 	}
+
 	private static List<Handler> handlers = new ArrayList<>();
 	static {
-		//Logger.getGlobal().setLevel(Level.FINEST);
-		//Logger.getGlobal().addHandler(new SystemOutHandler());
 		if (System.getProperty("java.util.logging.config.file") == null) {
-			//System.setProperty("java.util.logging.config.file", path);
-			String level = System.getProperty("log.level", "info");
-		      try
-		      {
-		          LogManager.getLogManager().readConfiguration(Log.class.getResourceAsStream("logging."+level+".properties"));
-		      }
-		      catch (final IOException e)
-		      {
-		          Logger.getAnonymousLogger().severe("Could not load default logging.properties file");
-		          Logger.getAnonymousLogger().severe(e.getMessage());
-		      }
-		} else System.out.println("java.util.logging.config.file="+System.getProperty("java.util.logging.config.file"));
+			String levelPropertyValue = System.getProperty("log.level", "info");
+			String level = Arrays.
+					asList("debug", "info", "warning").stream().
+					filter(l -> levelPropertyValue.equals(l)).
+					findAny().
+					orElse("info");
+			try {
+				LogManager.getLogManager().readConfiguration(Log.class.getResourceAsStream("logging." + level + ".properties"));
+			} catch (final IOException e) {
+				Logger.getAnonymousLogger().severe("Could not load default logging.properties file");
+				Logger.getAnonymousLogger().severe(e.getMessage());
+			}
+		}
 		handlers.add(new SystemOutHandler());
 	}
+
 	public static void addHandler(Handler handler) {
 		handlers.add(handler);
 	}
+
 	public static Logger get() {
 		StackTraceElement[] el = Thread.currentThread().getStackTrace();
-		Logger logger = Logger.getLogger(el[2].getClassName());		
-		//logger.addHandler(new SystemOutHandler());
+		Logger logger = Logger.getLogger(el[2].getClassName());
 		logger.setUseParentHandlers(false);
-		//consoleHandler.setLevel(Level.INFO);
-		if (logger.getHandlers().length == 0) {			
+		if (logger.getHandlers().length == 0) {
 			for (Handler handler : handlers) {
 				logger.addHandler(handler);
 			}
 		}
-		
+
 		return logger;
 	}
+
 	public static class SystemOutHandler extends ConsoleHandler {
-	  public SystemOutHandler() {
-	   // setOutputStream(System.out);
-	    setFormatter(formatter);
-	    setLevel(Level.FINE);
-	  }
-	  @Override
-		public void publish(LogRecord record) {
-			// TODO Auto-generated method stub
-			super.publish(record);
+		public SystemOutHandler() {
+			setFormatter(formatter);
+			setLevel(Level.FINE);
 		}
 	}
 }
